e0768c7dbfe56bdd0822a74ee237816af659f939
Arthur Santos <arthurldsantos@gmail.com>
2024-12-02 17:10:41 -0300

    alterar data ao editar um item

--- js/editarItem.js
+++ js/editarItem.js
@@ -1,3 +1,5 @@
+import { obterDataAtual } from "./obterDataAtual.js";
+
 export const editarItem = (elemento) => {
     let novoItem = prompt('Digite o novo nome do item:');
 
@@ -12,5 +14,8 @@ export const editarItem = (elemento) => {
             elemento.querySelector('.checkbox-customizado').classList.add('checked');
             itemTextoAtualizado.style.textDecoration = 'line-through';
         }
+
+        const dataDeCriacao = elemento.querySelector('.item-lista-texto');
+        dataDeCriacao.textContent = obterDataAtual();
     }
 }
--- js/verificarListaVazia.js
+++ js/verificarListaVazia.js
@@ -1,7 +1,6 @@
 const mensagemListaVazia = document.getElementById('mensagem-lista-vazia');
 
 export function verificarListaVazia(lista) {
-    console.log(lista);
     if (lista.querySelectorAll('li').length === 0) {
         mensagemListaVazia.style.display = 'block';
     } else {
